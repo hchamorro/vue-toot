3a0ce3739851d6cd61bea325177d93204dfb5b5b
Hannah Chamorro <hannah.chamorro@gmail.com>
2021-01-28 19:34:51 -0600

    slots and props

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
+<div>
 <h1> {{ title }} </h1>
+<div v-if="showModal"> 
+ <!-- prop drive :header="header" :text="text" -->
+<Modal theme="sales" @close="toggleModal">
+  <template v-slot:links>
+    <a href="#"> sign up</a>
+
+    <a href="#"> more info </a>
+  </template>
+  <h1>Slot!</h1>
+  <p> slot content </p>
+  
+</Modal>>
+</div>
+<br>
+<button @click.alt="toggleModal"> show modal (alt) </button>
+</div>
 </template>
 
 <script>
 
+import Modal from './components/Modal.vue'
+
 export default {
+  components: { Modal },
   name: 'App',
   data() {
     return {
-      title: 'My first vue app'
+      title: 'Vue App',
+      header:"Woo header",
+      text: "Prop driving the prop attribute",
+      showModal: false
     }
+  },
+  methods: {
+    toggleModal(){
+    this.showModal = !this.showModal
+  }
   }
 }
 </script>
